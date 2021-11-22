a43a555900db3f6f4090a001f3a0123d57ae8653
nguyenhuudungz <nguyenhuudungz@yahoo.com>
2021-11-22 17:49:24 +0700

    Done step 2

--- src/getRpcUrl.ts
+++ src/getRpcUrl.ts
@@ -1,14 +1,4 @@
-import sample from 'lodash/sample';
-
-if (
-  process.env.NODE_ENV !== 'production' &&
-  (!process.env.REACT_APP_NODE_1 || !process.env.REACT_APP_NODE_2 || !process.env.REACT_APP_NODE_3)
-) {
-  throw Error('One base RPC URL is undefined');
-}
-
-// Array of available nodes to connect to
-export const nodes = [process.env.REACT_APP_NODE_1, process.env.REACT_APP_NODE_2, process.env.REACT_APP_NODE_3];
+export const nodes = [process.env.REACT_APP_NODE];
 
 const getNodeUrl = () => {
   // Use custom node if available (both for development and production)
@@ -16,7 +6,7 @@ const getNodeUrl = () => {
   if (process.env.REACT_APP_NODE_PRODUCTION) {
     return process.env.REACT_APP_NODE_PRODUCTION;
   }
-  return sample(nodes);
+  return nodes[0];
 };
 
 export default getNodeUrl;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
-import { StrictMode, useEffect, useState } from 'react';
+import { StrictMode, useEffect, useMemo, useRef, useState } from 'react';
 import ReactDOM from 'react-dom';
 import { InjectedConnector } from '@web3-react/injected-connector';
-import { UnsupportedChainIdError, useWeb3React, Web3ReactProvider } from '@web3-react/core';
+import { UnsupportedChainIdError, Web3ReactProvider } from '@web3-react/core';
 import { ethers } from 'ethers';
-import { setupNetwork } from './wallet';
+import { setupNetwork } from 'wallet';
 import BigNumber from 'bignumber.js';
+import simpleRpcProvider from 'providers';
+import bep20Abi from 'erc20.json';
+import useActiveWeb3React from 'useActiveWeb3React';
+import { isAddress } from 'ethers/lib/utils';
 
 BigNumber.config({
   EXPONENTIAL_AT: 1000,
@@ -13,16 +17,46 @@ BigNumber.config({
 
 const injected = new InjectedConnector({ supportedChainIds: [97] });
 
+const getContract = (abi: any, address: string, signer?: ethers.Signer | ethers.providers.Provider) => {
+  const signerOrProvider = signer ?? simpleRpcProvider;
+  return new ethers.Contract(address, abi, signerOrProvider);
+};
+
+export const getBep20Contract = (address: string, signer?: ethers.Signer | ethers.providers.Provider) => {
+  return getContract(bep20Abi, address, signer);
+};
+
+export const useERC20 = (address: string) => {
+  const { library, account } = useActiveWeb3React();
+  return useMemo(
+    () => (address && library ? getBep20Contract(address, account ? library.getSigner() : undefined) : undefined),
+    [account, address, library]
+  );
+};
+
 function App() {
   // setup
-  const { activate, account, library } = useWeb3React();
+  const { activate, account, library } = useActiveWeb3React();
   // b1
   const [balance, setBalance] = useState('');
   // b2
+  const scaRef = useRef<HTMLInputElement>(null);
   const [sca, setSca] = useState('');
+  const contract = useERC20(sca);
+  const [contractIn4, setContractIn4] = useState<any>({});
+
+  useEffect(() => {
+    (async () => {
+      if (contract) {
+        const [name, symbol, decimals] = await Promise.all([contract.name(), contract.symbol(), contract.decimals()]);
+        setContractIn4({ name, symbol, decimals });
+      }
+    })();
+  }, [contract]);
+
   // b3
-  const [ra, setRa] = useState('');
-  const [amount, setAmount] = useState('');
+  const raRef = useRef<HTMLInputElement>(null);
+  const amountRef = useRef<HTMLInputElement>(null);
   // b4
   const [txStatus, setTxStatus] = useState([]);
 
@@ -39,7 +73,6 @@ function App() {
 
   useEffect(() => {
     (async () => {
-      // const provider = new ethers.providers.Web3Provider(window.ethereum as any);
       if (account && library) {
         const signer = await library.getSigner();
         const b = await signer.getBalance();
@@ -66,16 +99,54 @@ function App() {
 
       <hr />
 
-      <h1>b2: input smart contract address, output its information</h1>
-      <input type="text" placeholder="Smart contract address" value={sca} onChange={(e) => setSca(e.target.value)} />
-      <button onClick={() => {}}>show smart contract info</button>
+      <h1>b2: input bep20 smart contract address, output its information</h1>
+      <input type="text" placeholder="Smart contract address" ref={scaRef} />
+      <p>
+        Example:{' '}
+        <a href="https://testnet.bscscan.com/token/0x82f1ffcdb31433b63aa311295a69892eebcdc2bb">
+          0x82f1ffcdb31433b63aa311295a69892eebcdc2bb
+        </a>
+      </p>
+      <p>name: {contractIn4.name ?? '--'}</p>
+      <p>symbol: {contractIn4.symbol ?? '--'}</p>
+      <p>decimals: {contractIn4.decimals ?? '--'}</p>
+      <button
+        onClick={() => {
+          if (scaRef.current) {
+            if (isAddress(scaRef.current.value)) {
+              setSca(scaRef.current.value);
+            } else {
+              alert('Address not valid.');
+            }
+          }
+        }}
+      >
+        show smart contract info
+      </button>
 
       <hr />
 
       <h1>b3: send BNB to another address</h1>
-      <input type="text" placeholder="Recipient address" value={ra} onChange={(e) => setRa(e.target.value)} />
-      <input type="number" placeholder="Amount" value={amount} onChange={(e) => setAmount(e.target.value)} />
-      <button onClick={() => {}}>send</button>
+      <input type="text" placeholder="Recipient address" ref={raRef} />
+      <input type="number" placeholder="Amount" disabled={!balance} ref={amountRef} />
+      <button
+        onClick={() => {
+          if (amountRef.current && raRef.current) {
+            const ra = raRef.current.value;
+            const amount = new BigNumber(amountRef.current.value);
+            if (!isAddress(ra)) {
+              alert('Invalid address.');
+              return;
+            }
+            if (amount.isGreaterThan(balance)) {
+              alert('Value cannot greater than balance.');
+              return;
+            }
+          }
+        }}
+      >
+        send
+      </button>
 
       <hr />
 
