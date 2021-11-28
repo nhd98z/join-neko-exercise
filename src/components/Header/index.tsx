bae9d93744f5f7688dca547149bbbe4626db5142
nguyenhuudungz <nguyenhuudungz@yahoo.com>
2021-11-28 13:39:10 +0700

    Add more content in homepage

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,64 @@
+import { Box, Flex } from 'components/Box';
+import { Text } from 'components/Text';
+import styled from 'styled-components';
+import LogoText from 'assets/logo-text.svg';
+import { BsChevronDown } from 'react-icons/all';
+
+const FixedBox = styled(Box)`
+  position: fixed;
+  z-index: 1;
+  width: 100%;
+`;
+
+const MenuItem = ({ text }: { text: string }) => (
+  <Flex alignItems="center" tabIndex={0} style={{ cursor: 'pointer' }}>
+    <Text fontSize="20px" mr="8px">
+      {text}
+    </Text>
+    <BsChevronDown />
+  </Flex>
+);
+
+const ButtonBase = styled(Flex)`
+  padding: 8px 34px;
+  border-radius: 34px;
+  border: 1px solid #ffffff;
+  cursor: pointer;
+  font-size: 20px;
+  font-weight: 600;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ButtonDocs = styled(ButtonBase)`
+  background: transparent;
+`;
+
+const ButtonDownload = styled(ButtonBase)`
+  background: #ffffff;
+  color: #16001e;
+`;
+
 export default function Header() {
-  return <div style={{ position: 'fixed' }}>Header</div>;
+  return (
+    <FixedBox>
+      <Flex p="27px 205px 27px 95px" justifyContent="space-between">
+        <Flex style={{ gap: '60px' }}>
+          <img src={LogoText} alt="logo-text" />
+          <MenuItem text="EN" />
+        </Flex>
+        <Flex style={{ gap: '60px' }}>
+          <Flex style={{ gap: '40px' }}>
+            <MenuItem text="Features" />
+            <MenuItem text="About" />
+            <MenuItem text="Learn" />
+          </Flex>
+          <Flex style={{ gap: '20px' }}>
+            <ButtonDocs tabIndex={0}>Docs</ButtonDocs>
+            <ButtonDownload tabIndex={0}>Download</ButtonDownload>
+          </Flex>
+        </Flex>
+      </Flex>
+    </FixedBox>
+  );
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,34 +1,73 @@
-import { Box } from 'components/Box';
+import { Box, Flex } from 'components/Box';
+import { Text } from 'components/Text';
+
 import Background from 'assets/background.svg';
 import PhoneHolding from 'assets/phone-holding.png';
+import GamersDraft from 'assets/gamers-draft.svg';
+import GGPlay from 'assets/gg-play.png';
+import AppStore from 'assets/app-store.png';
+
+const AbsoluteBackground = () => (
+  <Box
+    style={{
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      backgroundImage: `url('${Background}')`,
+      backgroundSize: '100%',
+      backgroundRepeat: 'no-repeat',
+      width: '100%',
+      height: '100%',
+    }}
+  />
+);
+
+const AbsolutePhoneHolding = () => (
+  <img
+    src={PhoneHolding}
+    alt="phone-holding"
+    style={{
+      position: 'absolute',
+      top: 0,
+      right: 0,
+      width: 'auto',
+      height: '100vh',
+    }}
+  />
+);
 
 export default function Home() {
   return (
     <Box>
-      <Box
-        style={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          backgroundImage: `url('${Background}')`,
-          backgroundSize: '100%',
-          backgroundRepeat: 'no-repeat',
-          width: '100%',
-          height: '100%',
-        }}
-      />
-      <img
-        src={PhoneHolding}
-        alt="phone-holding"
-        style={{
-          position: 'absolute',
-          top: 0,
-          right: 0,
-          width: 'auto',
-          height: '100vh',
-        }}
-      />
-      <Box />
+      <AbsoluteBackground />
+      <AbsolutePhoneHolding />
+      <Box style={{ position: 'absolute', left: '95px', top: '30%' }}>
+        <Text fontSize="56px" fontWeight={700} lineHeight={1.25}>
+          Gaming Mode
+        </Text>
+        <Text fontSize="56px" fontWeight={700} lineHeight={1.25}>
+          Built by <span style={{ color: '#FC7819' }}>Gamers</span>
+        </Text>
+        <Text fontSize="56px" fontWeight={700} lineHeight={1.25}>
+          for&nbsp;
+          <span style={{ position: 'relative' }}>
+            Gamers
+            <img
+              src={GamersDraft}
+              style={{ position: 'absolute', top: '0', left: '-10px', height: '100%', maxWidth: 'unset' }}
+              alt="gamers-draft"
+            />
+          </span>
+        </Text>
+        <Flex mt="60px" style={{ gap: '20px' }}>
+          <Box tabIndex={0} style={{ cursor: 'pointer' }}>
+            <img src={GGPlay} alt="gg-play" />
+          </Box>
+          <Box tabIndex={0} style={{ cursor: 'pointer' }}>
+            <img src={AppStore} alt="app-store" />
+          </Box>
+        </Flex>
+      </Box>
     </Box>
   );
 }
